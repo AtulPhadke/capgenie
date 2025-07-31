23df82838ac62a60994cdd23049a887601f0b1db
Atul Phadke <atulphadke@Atuls-Air.lan>
2025-07-31 18:50:00 -0400

    Simplify readchar hook to avoid metadata file issues

--- hook-readchar.py
+++ hook-readchar.py
@@ -1,20 +1,8 @@
 # PyInstaller hook for readchar
-from PyInstaller.utils.hooks import collect_data_files, collect_submodules
+from PyInstaller.utils.hooks import collect_submodules
 
 # Collect all submodules
 hiddenimports = collect_submodules('readchar')
 
-# Collect data files if any
-datas = collect_data_files('readchar')
-
-# Add specific metadata handling
-try:
-    import readchar
-    import pkg_resources
-    readchar_dist = pkg_resources.get_distribution('readchar')
-    if readchar_dist.has_metadata('METADATA'):
-        datas.append((readchar_dist.get_metadata('METADATA'), 'readchar'))
-    elif readchar_dist.has_metadata('PKG-INFO'):
-        datas.append((readchar_dist.get_metadata('PKG-INFO'), 'readchar'))
-except (ImportError, pkg_resources.DistributionNotFound):
-    pass 
+# No data files needed for readchar
+datas = [] 
