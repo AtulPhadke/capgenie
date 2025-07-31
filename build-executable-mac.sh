90fd127fe69ae12bd7c35eda3325ac2d90bc7cf3
Atul Phadke <atulphadke@Atuls-Air.lan>
2025-07-31 19:29:42 -0400

    Use correct PyInstaller command format with --hidden-import and --copy-metadata for readchar

--- build-executable-mac.sh
+++ build-executable-mac.sh
@@ -147,7 +147,7 @@ EOF
 
 # Build the executable
 print_status "Building executable with PyInstaller..."
-pyinstaller --clean "$SPEC_FILE"
+pyinstaller --clean --hidden-import=inquirer --hidden-import=readchar --copy-metadata readchar "$SPEC_FILE"
 
 # Test the executable
 print_status "Testing the executable..."
