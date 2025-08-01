9004298f76cd9d4640dde4ea40ea9236cfd80a46
Atul Phadke <atulphadke@Atuls-Air.lan>
2025-07-31 20:47:01 -0400

    Fix Windows batch file: remove spec file creation and use direct PyInstaller approach

--- build-executable-windows.bat
+++ build-executable-windows.bat
@@ -7,7 +7,6 @@ REM Configuration
 set SCRIPT_DIR=%~dp0
 set BUILD_DIR=%SCRIPT_DIR%build
 set DIST_DIR=%SCRIPT_DIR%dist
-set SPEC_FILE=%SCRIPT_DIR%capgenie.spec
 
 REM Check if we're in the correct directory
 if not exist "pyproject.toml" (
@@ -19,7 +18,6 @@ REM Clean previous builds
 echo [INFO] Cleaning previous builds...
 if exist "%BUILD_DIR%" rmdir /s /q "%BUILD_DIR%"
 if exist "%DIST_DIR%" rmdir /s /q "%DIST_DIR%"
-if exist "%SPEC_FILE%" del "%SPEC_FILE%"
 
 REM Install PyInstaller if not already installed
 echo [INFO] Installing PyInstaller...
@@ -28,94 +26,6 @@ pip install pyinstaller
 REM Install the package in development mode
 echo [INFO] Installing CapGenie in development mode...
 pip install -e .
-
-REM Create PyInstaller spec file
-echo [INFO] Creating PyInstaller spec file...
-(
-echo # -*- mode: python ; coding: utf-8 -*-
-echo from PyInstaller.utils.hooks import collect_data_files
-echo.
-echo block_cipher = None
-echo.
-echo a = Analysis(
-echo     ['src/capgenie/cli.py'],
-echo     pathex=[],
-echo     binaries=[],
-echo     datas=[
-echo         ^('src/capgenie', 'capgenie'^),
-echo         ^('assets', 'assets'^),
-echo     ] + collect_data_files^('inquirer'^) + collect_data_files^('readchar', include_py_files=True^) + collect_data_files^('Bio'^) + collect_data_files^('sklearn'^) + collect_data_files^('umap'^) + collect_data_files^('pyahocorasick'^) + collect_data_files^('logomaker'^),
-echo     hiddenimports=[
-echo         'capgenie.bubble',
-echo         'capgenie.biodistribution',
-echo         'capgenie.motif',
-echo         'capgenie.search_aav9',
-echo         'capgenie.enrichment',
-echo         'capgenie.spreadsheet',
-echo         'capgenie.mani',
-echo         'capgenie.denoise',
-echo         'capgenie.filter_module',
-echo         'capgenie.fuzzy_match',
-echo         'pandas',
-echo         'numpy',
-echo         'scipy',
-echo         'matplotlib',
-echo         'plotly',
-echo         'Bio',
-echo         'Bio.Seq',
-echo         'ahocorasick',
-echo         'sklearn',
-echo         'sklearn.cluster',
-echo         'umap',
-echo         'umap.umap_',
-echo         'logomaker',
-echo         'inquirer',
-echo         'inquirer.themes',
-echo         'inquirer.questions',
-echo         'inquirer.render',
-echo         'inquirer.render.console',
-echo         'inquirer.render.console._list',
-echo         'inquirer.render.console._text',
-echo         'inquirer.render.console._checkbox',
-echo         'inquirer.render.console._confirm',
-echo         'inquirer.render.console._password',
-echo         'inquirer.render.console._path',
-echo         'inquirer.render.console._editor',
-echo     ],
-echo     hookspath=[],
-echo     hooksconfig={},
-echo     runtime_hooks=[],
-echo     excludes=[],
-echo     win_no_prefer_redirects=False,
-echo     win_private_assemblies=False,
-echo     cipher=block_cipher,
-echo     noarchive=False,
-echo ^)
-echo.
-echo pyz = PYZ^(a.pure, a.zipped_data, cipher=block_cipher^)
-echo.
-echo exe = EXE^(
-echo     pyz,
-echo     a.scripts,
-echo     a.binaries,
-echo     a.zipfiles,
-echo     a.datas,
-echo     [],
-echo     name='capgenie',
-echo     debug=False,
-echo     bootloader_ignore_signals=False,
-echo     strip=False,
-echo     upx=True,
-echo     upx_exclude=[],
-echo     runtime_tmpdir=None,
-echo     console=True,
-echo     disable_windowed_traceback=False,
-echo     argv_emulation=False,
-echo     target_arch=None,
-echo     codesign_identity=None,
-echo     entitlements_file=None,
-echo ^)
-) > "%SPEC_FILE%"
 
 REM Build the executable
 echo [INFO] Building executable with PyInstaller...
